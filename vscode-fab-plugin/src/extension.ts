f34ae0b0abcf98ead46bb9b71291d78bd1e02a0a
Fabric.Offical <fabric.project.official@gmail.com>
2025-08-03 15:59:51 -0700

    Create extension.ts

---
+++ vscode-fab-plugin/src/extension.ts
@@ -0,0 +1,20 @@
+import * as vscode from 'vscode';
+import { exec } from 'child_process';
+
+export function activate(context: vscode.ExtensionContext) {
+    console.log('Fabric VSCode Plugin Activated');
+
+    const buildCmd = vscode.commands.registerCommand('fabric.build', () => runFabCommand('fab build'));
+    const auditCmd = vscode.commands.registerCommand('fabric.audit', () => runFabCommand('fab audit'));
+    const claimCmd = vscode.commands.registerCommand('fabric.claim', () => runFabCommand('fab claim'));
+
+    context.subscriptions.push(buildCmd, auditCmd, claimCmd);
+}
+
+function runFabCommand(command: string) {
+    const terminal = vscode.window.createTerminal('Fabric CLI');
+    terminal.sendText(command);
+    terminal.show();
+}
+
+export function deactivate() {}
