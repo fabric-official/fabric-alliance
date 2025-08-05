5376d30d8c5ca53e3f56847695a6ea5c3d1450cd
Fabric.Offical <fabric.project.official@gmail.com>
2025-08-05 11:31:38 -0700

    Update index.ts

--- vscode-fab-plugin/src/test/suite/index.ts
+++ vscode-fab-plugin/src/test/suite/index.ts
@@ -1,6 +1,6 @@
 import * as path from 'path';
 import Mocha from 'mocha';
-import glob from 'glob';
+import { glob } from 'glob';
 
 export function run(): Promise<void> {
   const mocha = new Mocha({
@@ -11,24 +11,23 @@ export function run(): Promise<void> {
   const testsRoot = path.resolve(__dirname, '..');
 
   return new Promise((resolve, reject) => {
-    glob('**/*.test.js', { cwd: testsRoot }, (err, files) => {
-      if (err) {
-        return reject(err);
-      }
+    glob('**/*.test.js', { cwd: testsRoot })
+      .then((files) => {
+        files.forEach((f) => mocha.addFile(path.resolve(testsRoot, f)));
 
-      files.forEach((f) => mocha.addFile(path.resolve(testsRoot, f)));
-
-      try {
-        mocha.run((failures) => {
-          if (failures > 0) {
-            reject(new Error(`${failures} tests failed.`));
-          } else {
-            resolve();
-          }
-        });
-      } catch (err) {
-        reject(err);
-      }
-    });
+        try {
+          mocha.run((failures) => {
+            if (failures > 0) {
+              reject(new Error(`${failures} tests failed.`));
+            } else {
+              resolve();
+            }
+          });
+        } catch (err) {
+          reject(err);
+        }
+      })
+      .catch((err) => reject(err));
   });
 }
+
