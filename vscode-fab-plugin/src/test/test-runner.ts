a657d4db07550d86c1ff6b3d24a131c76fb8a313
Fabric.Offical <fabric.project.official@gmail.com>
2025-08-05 12:56:33 -0700

    Update test-runner.ts

--- vscode-fab-plugin/src/test/test-runner.ts
+++ vscode-fab-plugin/src/test/test-runner.ts
@@ -1,15 +1,15 @@
 import * as path from 'path';
 import Mocha from 'mocha';
-import glob from 'glob';
+import * as glob from 'glob';
 
 async function run(): Promise<void> {
   const mocha = new Mocha({ ui: 'tdd', color: true });
   const testsRoot = path.resolve(__dirname);
 
   return new Promise((resolve, reject) => {
-    glob('**/*.test.js', { cwd: testsRoot }, (err, files) => {
+    (glob as any)('**/*.test.js', { cwd: testsRoot }, (err: any, files: string[]) => {
       if (err) return reject(err);
-      files.forEach(f => mocha.addFile(path.resolve(testsRoot, f)));
+      files.forEach((f: string) => mocha.addFile(path.resolve(testsRoot, f)));
       mocha.run(failures => failures > 0 ? reject(new Error(`${failures} tests failed.`)) : resolve());
     });
   });
@@ -19,3 +19,4 @@ run().catch(err => {
   console.error(err);
   process.exit(1);
 });
+
